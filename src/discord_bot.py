dfe230ae065b27fd7651cfabaee490d218fb65ad
angelinn <angelin.nedelchev@outlook.com>
2019-01-03 11:56:46 +0200

    Cleanup test code.

--- src/discord_bot.py
+++ src/discord_bot.py
@@ -4,11 +4,6 @@ import json
 import os
 import sys
 
-with open(os.path.join(sys.path[0], 'settings.json')) as f:
-    data = json.load(f)
-
-TOKEN = data['token']
-
 client = discord.Client()
 
 @client.event
@@ -21,16 +16,19 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if 'pleb' in message.content:
-        #counter = 0
-        #tmp = await client.send_message(message.channel, 'Calculating messages...')
-        #async for log in client.logs_from(message.channel, limit=100):
-        #    if log.author == message.author:
-        #        counter += 1
-
-        # await client.edit_message(tmp, 'You have {} messages.'.format(counter))
         await client.send_message(message.channel, 'Cura is.')
     elif message.content.startswith('!sleep'):
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'Done sleeping')
 
-client.run(TOKEN)
+def main():
+    with open(os.path.join(sys.path[0], 'settings.json')) as f:
+        data = json.load(f)
+
+    TOKEN = data['token']
+
+    client.run(TOKEN)
+
+
+if __name__ == '__main__':
+    main()
