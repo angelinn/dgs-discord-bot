5f5c56477d4987046dedc9fce02bff97b501b3e1
angelinn <angelin.nedelchev@outlook.com>
2019-01-20 22:40:26 +0200

    Move the repost logic to a separate class.

--- src/discord_bot.py
+++ src/discord_bot.py
@@ -3,11 +3,12 @@ import asyncio
 import json
 import os
 import sys
-import requests
-import hashlib
+
+from repost_manager import RepostManager
+
+repost_manager = RepostManager()
 
 client = discord.Client()
-image_hashes = set()
 
 @client.event
 async def on_ready():
@@ -16,56 +17,20 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
-def has_image(message):
-    return len(message.attachments) > 0
-
-def load_hashes():
-    if os.path.exists(os.path.join(sys.path[0], 'hashes.txt')):
-        with open(os.path.join(sys.path[0], 'hashes.txt')) as f:
-            lines = f.read().splitlines()
-            for hash in lines:
-                image_hashes.add(hash)
-
-def save_hashes():
-    with open(os.path.join(sys.path[0], 'hashes.txt'), 'w') as f:
-        for item in image_hashes:
-            f.write("%s\n" % item)
-
-def calculate_hash(bytes):
-    return hashlib.md5(bytes).hexdigest()
-
-def download_image(url):
-    return requests.get(url).content
-
-def is_repost(message):
-    if has_image(message):
-        url = message.attachments[0]['url']
-        img_data = download_image(url)
-        hash = calculate_hash(img_data)
-
-        return hash in image_hashes
-
-    return False
-
 @client.event
 async def on_message(message):
-    if is_repost(message):
+    if repost_manager.is_repost(message):
         try:
             await client.send_message(message.channel, message.author.display_name + ' just reposted a meme.')
             await client.delete_message(message)
         except discord.Forbidden:
             await client.send_message(message.channel, 'I need permissions to manage messages in order to work.')
-    else:
-        image_hashes.add(hash)
-        save_hashes()
 
 def main():
     with open(os.path.join(sys.path[0], 'settings.json')) as f:
         data = json.load(f)
 
     TOKEN = data['token']
-
-    load_hashes()
 
     client.run(TOKEN)
 
---
+++ src/repost_manager.py
@@ -0,0 +1,45 @@
+import requests
+import hashlib
+import os
+import sys
+
+class RepostManager:
+    def __init__(self):
+        self.image_hashes = set()
+        self.load_hashes()
+
+    def load_hashes(self):
+        if os.path.exists(os.path.join(sys.path[0], 'hashes.txt')):
+            with open(os.path.join(sys.path[0], 'hashes.txt')) as f:
+                lines = f.read().splitlines()
+                for hash in lines:
+                    self.image_hashes.add(hash)
+
+    def save_hashes(self):
+        with open(os.path.join(sys.path[0], 'hashes.txt'), 'w') as f:
+            for item in self.image_hashes:
+                f.write("%s\n" % item)
+
+    def has_image(self, message):
+        return len(message.attachments) > 0
+
+    def calculate_hash(self, bytes):
+        return hashlib.md5(bytes).hexdigest()
+
+    def download_image(self, url):
+        return requests.get(url).content
+
+    def is_repost(self, message):
+        if self.has_image(message):
+            url = message.attachments[0]['url']
+            img_data = self.download_image(url)
+            hash = self.calculate_hash(img_data)
+
+            reposted = hash in self.image_hashes
+            if not reposted:        
+                self.image_hashes.add(hash)
+                self.save_hashes()
+
+            return reposted
+
+        return False
